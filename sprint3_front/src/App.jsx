4e1a74b14eaaf1dbb47e07cc6707a480cb0c2699
henriquebrnetto <henriquebrnetto02@gmail.com>
2024-12-02 10:20:20 -0300

    minor appearance changes

--- sprint3_front/src/App.jsx
+++ sprint3_front/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true); // Simulação do estado de login
 
   return (
-    <AppBar position="fixed">
+    <AppBar position="fixed" sx={{ backgroundColor: "#A71220" }} >
       <Toolbar disableGutters>
         {/* Logo centralizado */}
         <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
@@ -18,7 +18,7 @@ function App() {
             href="/"
             sx={{
               fontWeight: 700,
-              letterSpacing: '.15rem',
+              letterSpacing: '.1rem',
               color: 'inherit',
               fontSize: { xs: '0.75rem', sm: '1.25rem', md: '1.75rem' }, // Tamanhos responsivos para o texto
               textAlign: 'center',
@@ -32,8 +32,8 @@ function App() {
         {isLoggedIn && (
           <Box sx={{ flexGrow: 0, mr: { xs: 1, sm: 2, md: 5 } }}>
             <Tooltip title="Logout">
-              <IconButton color="inherit" sx={{ fontSize: { xs: '1.5rem', sm: '2rem', md: '2.5rem' } }}>
-                <LogoutIcon />
+              <IconButton color="inherit">
+                <LogoutIcon sx={{ fontSize: { xs: '1rem', sm: '1.5rem', md: '1.75rem' } }} />
               </IconButton>
             </Tooltip>
           </Box>
